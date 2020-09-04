cea95f944ad21cc9ab8e3d278c4dc18297b33b75
jdh4 <jdh4@princeton.edu>
2020-09-04 00:17:50 -0400

    Added home directories

--- clusters_ls_home.sh
+++ clusters_ls_home.sh
@@ -7,3 +7,7 @@ done
 ls /home > ./tiger_ls.txt 2>/dev/null
 echo 'tiger_ls.txt'
 /usr/licensed/anaconda3/2020.7/bin/python combine_ls.py 2>/dev/null
+for cluster in adroit della perseus tigressdata traverse
+do
+  scp combined_ls.csv jdh4@${cluster}.princeton.edu:/home/jdh4/.lft/${cluster}_ls.txt /tigress/jdh4/python-devel/lft 2>/dev/null
+done
