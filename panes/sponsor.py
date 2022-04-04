c266d75d66e90abb1964ff976c30c49d9e3e0436
jdh4 <jdh4@princeton.edu>
2022-04-04 11:45:48 -0400

    Sponsor is now cluster-specific

--- panes/sponsor.py
+++ panes/sponsor.py
@@ -1,61 +1,74 @@
+import os
 import subprocess
-from panes import utils
+#import pandas as pd
 
-####################
-## sponsor_report ##
-####################
-
-# $ sponsor_report jdh4
-# No sponsored users for jdh4 (Jonathan D. Halverson)
-# This user is sponsored by curt
-
-# $ sponsor_report aturing
-# FATAL ERROR: Could not find user aturing in Princeton LDAP
-
-# $ sponsor_report hwaight
-# hwaight (Hannah C. Waight) has no Tigress account
-
-# $ sponsor_report ethier
-# Manager ethier (Stephane Ethier) has sponsored the following users
-#      gascione (George Ascione) has accounts on eddy
-#
-# Cluster eddy has these users gascione
-
-def get_sponsor(netid):
-
-  cmd = f"sponsor_report {netid}"
-  try:
-    output = subprocess.run(cmd, capture_output=True, shell=True, timeout=5)
-  except subprocess.TimeoutExpired:
-    sponsor = None
-  except:
-    # netid not in Princeton LDAP
-    sponsor = None
+def sponsor_per_cluster(netid, verbose=True):
+  """Sponsor found for all large clusters even if user does not have an account on a specific cluster."""
+  cmd = f"ldapsearch -x -H ldap://ldap1.rc.princeton.edu -b dc=rc,dc=princeton,dc=edu uid={netid} displayname manager description"
+  output = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True, timeout=5, text=True, check=True)
+  lines = output.stdout.split('\n')
+  if lines != [] and lines[-1] == "": lines = lines[:-1]
+  
+  # get primary manager (if more than 1 then take first)
+  line_index = 0
+  displayname = None
+  managers = []
+  for i, line in enumerate(lines):
+    if "displayname: " in line:
+      displayname = line.split(": ")[1].strip()
+    if "manager: " in line and "uid=" in line:
+      managers.append(line.split("uid=")[1].split(",")[0])
+      line_index = i
+  if managers == []:
+    primary = None
+    # try looking in CSV file if available
+    fname = "users_left_university_from_robert_knight.csv"
+    if os.path.exists(fname):
+      #rk = pd.read_csv(fname)
+      if not rk[rk.Netid_ == netid].empty:
+        primary = rk[rk.Netid_ == netid].Sponsor_Netid_.values[0]
+        if not displayname: displayname = rk[rk.Netid_ == netid].Name_.values[0]
+        if verbose: print(f"W: Primary sponsor for {netid} taken from CSV file.")
+    if not primary and verbose: print(f"W: No primary sponsor found for {netid} in CSES LDAP or CSV file.")
+  elif len(managers) > 1:
+    if verbose: print(f"W: User {netid} has multiple primary sponsors: {','.join(managers)}. Using {managers[0]}.")
+    primary = managers[0]
   else:
-    lines = output.stdout.decode("utf-8").split('\n')
-    if output.returncode == 1: return ("<NON-ASCII>", "<NON-ASCII>")
-    if len(lines) > 1 and 'No sponsored users' in lines[0]:
-      sponsor = lines[1].split()[-1]
-    elif "Manager" in lines[0] and "has sponsored" in lines[0]:
-      sponsor = netid
-    else:
-      sponsor = None
+    primary = managers[0]
+  if not displayname and verbose: print(f"W: Name not found for user {netid} in CSES LDAP.")
 
-  if sponsor:
-    accounts = []
-    cmd = f"sponsor_report {sponsor}"
-    try:
-      output = subprocess.run(cmd, capture_output=True, shell=True, timeout=5)
-    except:
-      pass
-    else:
-      lines = output.stdout.decode("utf-8").split('\n')
-      if output.returncode == 1: return ("<NON-ASCII>", "<NON-ASCII>")
-      for line in lines:
-        if "Manager" in line and "has sponsored" in line:
-          # Manager pdebene (Pablo G. Debenedetti) has sponsored the following users
-          sponsor_full_name = line.split('(')[1].split(')')[0]
-          sponsor_full_name = utils.remove_middle_initial(sponsor_full_name)
-          return (sponsor_full_name, sponsor)
-  else:
-    return (None, None)
+  # get all cluster-specific sponsors and name of user
+  sponsor = {"della":primary, "stellar":primary, "tiger":primary, "traverse":primary, "displayname":displayname}
+  s = ""
+  for line in lines[line_index:]:
+    s += line.strip() if not line.startswith("#") else ""
+  for cluster in sponsor.keys():
+    x = f"{cluster}:"
+    if x in s:
+      sponsor_netid = s.split(x)[1].split("=")[0]
+      if sponsor_netid == "USER":
+        if verbose: print(f"W: Sponsor entry of {sponsor_netid} found for {netid} on {cluster}. Corrected to {netid}.")
+        sponsor_netid = netid
+      if "(" in sponsor_netid:
+        tmp = sponsor_netid.split("(")[0].strip()
+        if verbose: print(f"W: Sponsor entry of {sponsor_netid} found for {netid} on {cluster}. Corrected to {tmp}.")
+        sponsor_netid = tmp
+      sponsor[cluster] = sponsor_netid
+
+  return sponsor
+
+
+def sponsor_full_name(netid, verbose=True):
+  """Return the full name of the sponsor for the given netid of the sponsor."""
+  cmd = f"ldapsearch -x -H ldap://ldap1.rc.princeton.edu -b dc=rc,dc=princeton,dc=edu uid={netid} displayname"
+  output = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True, timeout=5, text=True, check=True)
+  lines = output.stdout.split('\n')
+  if lines != [] and lines[-1] == "": lines = lines[:-1]
+
+  displayname = None
+  for line in lines:
+    if "displayname: " in line:
+      displayname = line.split(": ")[1].strip()
+  
+  if not displayname and verbose: print(f"W: Name not found in CSES LDAP for sponsor {netid}.")
+  return displayname
